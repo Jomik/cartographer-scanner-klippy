fd32b3805f77173c76aab37d08b135c040540dd7
Jonas Damtoft <Jomik@users.noreply.github.com>
2024-12-17 20:01:22 +0100

    feat: add scanner model

---
+++ cartographer/model.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, final
+from numpy.polynomial import Polynomial
+
+if TYPE_CHECKING:
+    from klippy.configfile import ConfigWrapper
+
+
+@final
+class ScannerModel:
+    @staticmethod
+    def load(config: ConfigWrapper):
+        name = config.get_name()
+        temp = config.getfloat("temperature")
+        coef = config.getfloatlist("coefficient")
+        domain = config.getfloatlist("domain", count=2)
+        [min_z, max_z] = config.getfloatlist("z_range", count=2)
+        checksum = config.get("checksum")
+        poly = Polynomial(coef, domain)
+        return ScannerModel(name, poly, temp, min_z, max_z, checksum)
+
+    def __init__(
+        self,
+        name: str,
+        poly: Polynomial,
+        temp: float,
+        min_z: float,
+        max_z: float,
+        checksum: str,
+    ):
+        self.name = name
+        self.poly = poly
+        self.min_z = min_z
+        self.max_z = max_z
+        self.temp = temp
+        self.checksum = checksum
