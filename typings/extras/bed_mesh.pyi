f2437e213156d669d1435ccf8ca17de402ac3ec7
Jonas Damtoft <Jomik@users.noreply.github.com>
2024-12-21 15:11:53 +0100

    fix: proper types for probe sessions

--- typings/extras/bed_mesh.pyi
+++ typings/extras/bed_mesh.pyi
@@ -1,6 +1,8 @@
 # https://github.com/Klipper3d/klipper/blob/master/klippy/extras/bed_mesh.py
 from typing import Literal, TypedDict
 
+type _Pos = list[float]
+
 class BedMeshError(Exception): ...
 
 class _Params(TypedDict):
@@ -17,7 +19,7 @@ class _Params(TypedDict):
 
 class ZMesh:
     def __init__(self, params: _Params, name: str | None) -> None: ...
-    def build_mesh(self, z_matrix: list[list[float]]) -> None: ...
+    def build_mesh(self, z_matrix: list[_Pos]) -> None: ...
 
 class BedMeshCalibrate:
     mesh_config: _Params
--- typings/extras/homing.pyi
+++ typings/extras/homing.pyi
@@ -1,9 +1,9 @@
 # https://github.com/Klipper3d/klipper/blob/master/klippy/extras/homing.py
 
+type _Pos = list[float]
+
 class PrinterHoming:
-    def probing_move(
-        self, mcu_probe: object, pos: list[float], speed: float
-    ) -> list[float]: ...
+    def probing_move(self, mcu_probe: object, pos: _Pos, speed: float) -> _Pos: ...
 
 class Homing:
     def set_homed_position(self, pos: list[float | None]) -> None: ...
